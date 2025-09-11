a0b68d4a987a1465b37e9aaf77aa70657ab6d877
Mapache <brandox1002@gmail.com>
2025-09-11 14:53:33 -0500

    audit implement

---
+++ src/main/java/com/mapache/Enotes_API_Service/config/AuditAwareConfig.java
@@ -0,0 +1,12 @@
+package com.mapache.Enotes_API_Service.config;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditAwareConfig implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        return Optional.of(1);
+    }
+}
