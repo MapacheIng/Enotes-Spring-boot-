740e351d06fa056b50b3f22f55fa5368df6e10a4
Mapache <brandox1002@gmail.com>
2025-11-10 15:24:28 -0500

    add jwt on spring security

--- src/main/java/com/mapache/Enotes_API_Service/service/JwtService.java
+++ src/main/java/com/mapache/Enotes_API_Service/service/JwtService.java
@@ -1,9 +1,14 @@
 package com.mapache.Enotes_API_Service.service;
 
 import com.mapache.Enotes_API_Service.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface JwtService {
 
     String generateToken(User user);
+
+    String extractUsername(String token);
+
+    Boolean validateToken(String token, UserDetails userDetails);
 
 }
--- src/main/java/com/mapache/Enotes_API_Service/service/impl/JwtServiceImpl.java
+++ src/main/java/com/mapache/Enotes_API_Service/service/impl/JwtServiceImpl.java
@@ -3,10 +3,12 @@ package com.mapache.Enotes_API_Service.service.impl;
 import com.mapache.Enotes_API_Service.entity.Role;
 import com.mapache.Enotes_API_Service.entity.User;
 import com.mapache.Enotes_API_Service.service.JwtService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
@@ -39,7 +41,7 @@ public class JwtServiceImpl implements JwtService {
                 .claims().add(claims)
                 .subject(user.getEmail())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + (60 * 60 * 10)))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5)) // Token valid for 5 minutes
                 .and()
                 .signWith(getKey())
                 .compact();
@@ -47,7 +49,45 @@ public class JwtServiceImpl implements JwtService {
 
     }
 
+    @Override
+    public String extractUsername(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+
+
+    @Override
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+        return username.equalsIgnoreCase(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getExpiration().before(new Date());
+    }
+
+
+    private String role(String token){
+        Claims claims = extractAllClaims(token);
+        return claims.get("role", String.class);
+    }
+
     private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(decryptKey(secretKey))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey decryptKey(String secretKey) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
