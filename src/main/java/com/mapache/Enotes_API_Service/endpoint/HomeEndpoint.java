f5f7441ad309c2dc2c47a338a1c3cb6c12cf0075
Mapache <brandox1002@gmail.com>
2026-03-25 17:04:18 -0500

    Swagger implementation

--- src/main/java/com/mapache/Enotes_API_Service/endpoint/HomeEndpoint.java
+++ src/main/java/com/mapache/Enotes_API_Service/endpoint/HomeEndpoint.java
@@ -2,6 +2,8 @@ package com.mapache.Enotes_API_Service.endpoint;
 
 import com.mapache.Enotes_API_Service.dto.PasswordResetRequest;
 import com.mapache.Enotes_API_Service.exception.ResourceNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
@@ -9,23 +11,28 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 
+@Tag(name = "Home", description = "All the Home APIs")
 @RequestMapping("/api/v1/home")
 public interface HomeEndpoint {
 
+    @Operation(summary = "Verify User Account Endpoint", description = "Verifies a user's account using a verification code sent to their email.")
     @GetMapping("/verify")
-    public ResponseEntity<?> verifyUserAccount(
+    ResponseEntity<?> verifyUserAccount(
             @RequestParam Integer uid,
             @RequestParam String code
     ) throws ResourceNotFoundException;
 
+    @Operation(summary = "Send Password Reset Email Endpoint", description = "Sends an email to the user with a link to reset their password.")
     @GetMapping("/send-email-reset")
     ResponseEntity<?> sendEmailForPasswordReset(@RequestParam String email, HttpServletRequest request)
             throws ResourceNotFoundException, MessagingException, UnsupportedEncodingException;
 
+    @Operation(summary = "Verify Password Reset Link Endpoint", description = "Verifies the password reset link using the user ID and verification code.")
     @GetMapping("/verify-pswd-link")
     ResponseEntity<?> verifyPasswordResetLink(@RequestParam Integer uid, @RequestParam String code)
             throws ResourceNotFoundException;
 
+    @Operation(summary = "Reset Password Endpoint", description = "Resets the user's password using the provided new password and verification code.")
     @PostMapping("/reset-pswd")
     ResponseEntity<?> resetPassword(@RequestBody PasswordResetRequest passwordResetRequest)
             throws ResourceNotFoundException;
--- src/main/java/com/mapache/Enotes_API_Service/endpoint/UserEndpoint.java
+++ src/main/java/com/mapache/Enotes_API_Service/endpoint/UserEndpoint.java
@@ -1,19 +1,24 @@
 package com.mapache.Enotes_API_Service.endpoint;
 
 import com.mapache.Enotes_API_Service.dto.PasswordChangeRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name = "User", description = "User operation APIs")
 @RequestMapping("/api/v1/user")
 public interface UserEndpoint {
 
+    @Operation(summary = "Get user profile", description = "Get the profile information of the authenticated user")
     @GetMapping("/profile")
-    public ResponseEntity<?> getProfile();
+    ResponseEntity<?> getProfile();
 
+    @Operation(summary = "Change user password", description = "Change the password of the authenticated user")
     @PostMapping("/chng-pswd")
-    public ResponseEntity<?> changePassword(@RequestBody PasswordChangeRequest passwordRequest);
+    ResponseEntity<?> changePassword(@RequestBody PasswordChangeRequest passwordRequest);
 
 }
